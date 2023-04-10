ca5b60fdd4eb57651fe1fbd02e0d7f2fcec98399
Decimo <marlonadiguemartin548@gmail.com>
2023-04-10 16:11:54 +0800

    fixed : add html entity code to the text

--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { header } from '../../../public/images'
 import { Section } from '../shared'
 
-const HomeSection = () => {
+const HomeSection: React.FC = () => {
   return (
     <Section layers='00' layersStyles='' title='Environmental Preservative' image={header} alt='header-img' classname='h-screen' headerStyles='xl:mr-[7rem] 2xl:mr-[23rem]'>
     </Section>
