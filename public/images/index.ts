4a0bbdfc29682c0686fe0d9eda02eee1347abdc1
Decimo <marlonadiguemartin548@gmail.com>
2023-04-10 13:03:47 +0800

    update : added default exports

---
+++ public/images/index.ts
@@ -0,0 +1,14 @@
+import header from './header.webp'
+import p2 from './p2.webp'
+import p3 from './p3.webp'
+import p4 from './p4.webp'
+import p5 from './p5.webp'
+
+
+export {
+    header,
+    p2,
+    p3,
+    p4,
+    p5
+}
