8ee5de734e7245ab59bfec18900f3a50771f9a11
Decimo <marlonadiguemartin548@gmail.com>
2023-04-10 16:04:03 +0800

    update : add new config

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Archivo } from 'next/font/google'
+import { Header } from '../shared'
 
 const font = Archivo ({ subsets : ['latin'] })
 
@@ -9,7 +10,8 @@ interface LayoutProps {
 
 const Layout: React.FC <LayoutProps> = ({children}) => {
   return (
-    <main className='p-[1rem] w-full h-full m-auto text-[1rem]' id={font.className}>
+    <main className='p-[1rem] w-full h-full m-auto text-[1rem] xl:pr-0' id={font.className}>
+      <Header/>
         {children}
     </main>
   )
