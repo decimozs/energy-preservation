6b46027c2da19b532c34ffbecc67e0fe1b40d1f0
Decimo <marlonadiguemartin548@gmail.com>
2023-04-10 14:20:20 +0800

    fixed : responsiveness on different screens

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -5,10 +5,9 @@ const headerFont = Playfair_Display({ subsets: ['latin-ext']})
 
 const Header: React.FC = () => {
   return (
-    <header>
-        <p className={headerFont.className}>
-            Environmental Preservation
-        </p>
+    <header className='w-full max-w-[240px] lg:fixed lg:top-0 lg:left-0 lg:p-[1rem]'>
+      <h1 className={`${headerFont.className} font-bold text-[2rem] mb-[0.5rem]`}>environ</h1>
+      <p>An academic multimodal advocacy campaign for environmental preservation.</p>
     </header>
   )
 }
