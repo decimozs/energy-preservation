f2b5116ced5f6f27b0b64732fc1cd52ee253c945
Decimo <marlonadiguemartin548@gmail.com>
2023-04-10 16:04:33 +0800

    fixed : remove unnecessary codes

--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { Header, Title } from '../shared'
-import Image from 'next/image'
 import { header } from '../../../public/images'
+import { Section } from '../shared'
 
 const HomeSection = () => {
   return (
-    <section className='flex flex-col w-full h-screen'>
-      <Header/>
-      <div className='mt-auto flex flex-col items-end'>
-        <span className='text-[3rem] font-light mb-[1rem] lg:absolute lg:bottom-[1rem] lg:left-[1rem] lg:text-[6rem]'>01</span>
-        <Title/>
-        <div className='my-[1rem] relative w-full h-[400px] xl:h-[560px] xl:w-[950px]'>
-          <Image src={header} alt="header-img" layout='fill'></Image>
-        </div>
-      </div>
-    </section>
+    <Section layers='00' layersStyles='' title='Environmental Preservative' image={header} alt='header-img' classname='h-screen' headerStyles='xl:mr-[7rem] 2xl:mr-[23rem]'>
+    </Section>
   )
 }
 
--- src/components/sections/HomeSectionContext.tsx
+++ src/components/sections/HomeSectionContext.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const HomeSectionContext = () => {
   return (
-    <div className='flex flex-col gap-2 xl:gap-[3rem] xl:flex-row xl:justify-end '>
-        <p className='xl:w-[500px]'>An essential part of defending the environment and creating a sustainable future is environmental activism. Due to human activity, the environment is suffering serious problems like climate change, pollution, deforestation, and biodiversity loss. Promoting sustainable behaviors, lowering our environmental effect, and protecting the ecosystems and natural resources that support life on Earth.</p>
+    <div className='mb-[6rem] flex flex-col gap-2 xl:gap-[3rem] xl:justify-end xl:items-end '>
+        <p className='xl:w-[755px] xl:font-medium xl:text-[2.3rem]'>Advocating for the protection and conservation of the natural environment is known as environmental preservation. It is more crucial than ever to spread awareness and take action to protect our planet's natural resources in light of mounting worries about climate change, deforestation, pollution, and other environmental crises..</p>
 
-        <p className='xl:w-[400px]'>We can inform people about the effects of their decisions and encourage them to take action to lessen their environmental impact through education and awareness initiatives. Additionally, advocacy work may entail advocating for laws that safeguard the environment, such as restrictions on pollution or policies that support renewable energy sources. We can make the world more sustainable and resilient for future generations by banding together to promote environmental preservation.
-        </p>
+        <p className='xl:w-[400px] xl:mt-[-2rem]'>By standing up for environmental preservation, one ensures that the world can continue to support life in all of its forms. We may promote the adoption of sustainable practices and increase public understanding of environmental challenges through advocacy, activism, and education. We can improve the environment and ensure a better future for ourselves and future generations by banding together.</p>
     </div>
   )
 }
